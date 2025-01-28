25406b8d43f22d3350c10ffe2d3e020269d0a8d9
Svenvdm <svenvdm2013@gmail.com>
2025-01-28 12:49:42 +0100

    add allele endpoints

--- endpoints.py
+++ endpoints.py
@@ -11,3 +11,16 @@ class VariantEndpoint(Enum):
     SPDI_IMPACT = "variants/spdi/{spdi}/impact"
     SPDI_FREQUENCY = "variants/spdi/{spdi}/variant-frequency"
 
+
+class AlleleEndPoint(Enum):
+    ALL = "alleles"
+    ACTIVE = "alleles/list"
+    IDENTIFIER = "alleles/{identifier}" # returns a single allele by identifier (PV_ID or allele name)
+    ALLELE_NAME = "alleles/{identifier}/allele-name"
+    EVIDENCE_LEVEL = "alleles/{identifier}/evidence_level"
+    function = "alleles/{identifier}/function"
+    PV_ID = "alleles/{identifier}/pvid"
+    REFERENCES = "alleles/{identifier}/references"
+    VARIANTS = "alleles/{identifier}/variants"
+
+
