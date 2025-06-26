e9c55d66520d6f17ca60b8b92c38a1dc3e04d5d1
Svenvdm <svenvdm2013@gmail.com>
2025-06-26 12:02:41 +0200

    add ParameterValidationError exception

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -13,8 +13,10 @@ class NoDataFoundError(Exception):
         self.message = message
         super().__init__(self.message)
 
-class ValidationError(Exception):
-    """Base class for validation errors"""
-    def __init__(self, message):
+class ParameterValidationError(PharmVarApiException):
+    """Exception raised when parameter validation fails"""
+    def __init__(self, parameter: str, value: any, message: str):
+        self.parameter = parameter
+        self.value = value
         self.message = message
-        super().__init__(self.message)
+        super().__init__(f"{message}. Input value was: {value}")
