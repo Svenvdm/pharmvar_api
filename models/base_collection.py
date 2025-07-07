63d14b19d60410ec3c50cddad73336e4fac6a1e4
Svenvdm <svenvdm2013@gmail.com>
2025-07-07 23:07:55 +0200

    remove unused dunder method that was causing issues for Gene Class instantiation

--- models/base_collection.py
+++ models/base_collection.py
@@ -1,12 +1,6 @@
-from abc import ABC, abstractmethod
-from exceptions import ValidationError
-from enums.enums import FilterError
+from abc import ABC
+from exceptions.exceptions import ParameterValidationError
 class BaseCollection(ABC):
-
-    @abstractmethod
-    def _get_items(self):
-        pass
-
     def filter(self, **kwargs):
         """
         Filter the items in the collection by the given keyword arguments.
@@ -15,7 +9,7 @@ class BaseCollection(ABC):
         """
         invalid_attrs = self._get_invalid_attrs(kwargs)
         if invalid_attrs:
-            raise ValidationError(f"Invalid attribute {invalid_attrs} for {self.filter.__name__}")
+            raise ParameterValidationError(f"Invalid attribute {invalid_attrs} for {self.filter.__name__}")
         return self._filter_items(kwargs)
 
     def _get_invalid_attrs(self, kwargs):
